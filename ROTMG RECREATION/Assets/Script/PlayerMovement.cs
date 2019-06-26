fd2ceeec1e0dd3b836bb195754c93bb9dc62b5f8
angusthefilthyteemomain.github.io <angusozzie@gmail.com>
2019-06-26 10:08:40 -0700

    A new and actual unity game project

---
+++ ROTMG RECREATION/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidBody.MovePosition(
+             transform.position + change * speed * Time.deltaTime
+        );
+    }
+}
